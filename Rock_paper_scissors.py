dbaf46d6fe3acd8f0e8a9a1c2d243d3f849bf8c8
Abdi Deresa <Abdishegute@gmail.com>
2024-08-19 18:21:20 -0700

    added the logics

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -6,6 +6,12 @@ computer = random.choice(choices)
 player = None
 while player not in choices:
     player = input("rock, paper, or scissors?: ").lower()
-
-print("computer: ", computer)
-print("player: ", player)
+if player == computer:
+    print("computer: ", computer)
+    print("player: ", player)
+    print("Tie!")
+elif player == "rock":
+    if computer=="paper":
+    print("computer: ", computer)
+    print("player: ", player)
+    print("Tie!")
